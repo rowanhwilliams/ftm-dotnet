bc790b56598172b840019ebeea094efb4767c221
Rowan Williams <rowanhwilliams@hotmail.com>
2016-02-16 23:58:18 +0000

    Initialisation

---
+++ Fintechmonitor/Fintechmonitor.Repository/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fintechmonitor.Domain;
+
+namespace Fintechmonitor.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company GetById(int companyId);
+    }
+}
---
+++ Fintechmonitor/Fintechmonitor.Services/ICompanyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fintechmonitor.Domain;
+
+
+namespace Fintechmonitor.Services
+{
+    public interface ICompanyService
+    {
+        Company Company(int companyId);
+    }
+}
