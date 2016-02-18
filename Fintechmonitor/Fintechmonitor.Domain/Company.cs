072fe93b5cdfc0877bcc49bc8573652feda8dd3f
rowanhwilliams <rowanhwilliams@hotmail.com>
2016-02-18 13:01:10 +0000

    Update Company.cs

--- Fintechmonitor/Fintechmonitor.Domain/Company.cs
+++ Fintechmonitor/Fintechmonitor.Domain/Company.cs
@@ -10,6 +10,7 @@ namespace Fintechmonitor.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string City { get; set; }
         public string AboutUs { get; set; }
     }
 }
